f18654487085dceddba7d45b7ec16947efd284a7
julientregoat <julienctregoat@gmail.com>
2017-12-12 15:50:51 -0500

    updated

--- bin/run.rb
+++ bin/run.rb
@@ -5,4 +5,8 @@ require_relative "../lib/command_line_interface.rb"
 
 welcome
 character = get_character_from_user
-show_character_movies(character)
+while character.downcase != "exit"
+  show_character_movies(character)
+  character = get_character_from_user
+end
+goodbye
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -3,6 +3,10 @@ def welcome
 end
 
 def get_character_from_user
-  puts "please enter a character"
+  puts "please enter a character, or 'exit' to exit."
   gets.chomp.downcase
+end
+
+def goodbye
+  puts "May the force be with you."
 end
