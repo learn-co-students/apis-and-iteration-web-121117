a8f674db3b09bcb736913ab6da04c334012162d0
github username <github email address>
2017-12-12 15:50:45 -0500

    done

--- bin/run.rb
+++ bin/run.rb
@@ -6,3 +6,5 @@ require_relative "../lib/command_line_interface.rb"
 welcome
 character = get_character_from_user
 show_character_movies(character)
+movie = movie_prompt
+get_movie_info(movie)
--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,31 +3,75 @@ require 'json'
 require 'pry'
 
 def get_character_movies_from_api(character)
-  #make the web request
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
-  
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+  current_char = []
+  idx = 1
+  while current_char == []
+    all_characters = RestClient.get("https://www.swapi.co/api/people/?page=#{idx}")
+    character_hash = JSON.parse(all_characters)
+    current_char = find_char(character_hash, character)
+    idx += 1
+    if character_hash["next"] == nil
+      break
+    end
+  end
+    if current_char != []
+      current_char[0]["films"]
+    else
+      return []
+  end
+end
+
+def find_char(character_hash, character)
+  character_hash["results"].select do |currentCharacter|
+    currentCharacter["name"].downcase == character
+  end
 end
 
 def parse_character_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+  films_hash.map do |film|
+    filmHash = RestClient.get(film)
+    JSON.parse(filmHash)
+  end
 end
 
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
-  parse_character_movies(films_hash)
+  if films_hash == []
+    puts "There was no character found with that name."
+    return "There was no character found with that name."
+  else
+    info = parse_character_movies(films_hash)
+    filmsList = []
+    info.each do |filmStats|
+      filmsList << filmStats["title"]
+    end
+    end
+  puts filmsList
 end
 
-## BONUS
+#start bonus
 
+def get_movie_info(movie)
+  all_movies = RestClient.get('http://www.swapi.co/api/films/')
+  movie_hash = JSON.parse(all_movies)
+  current_movie = find_movie(movie_hash, movie)
+  if current_movie == []
+    puts "This is not a movie."
+    return ""
+  else
+    current_movie[0].each do |key, value|
+      puts "#{key}: #{value}"
+    end
+  end
+end
+
+def find_movie(movie_hash, movie)
+  selectedMovie = movie_hash['results'].select do |current_movie|
+    #binding.pry
+    current_movie['title'].downcase == movie
+  end
+end
+
+# BONUS
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
