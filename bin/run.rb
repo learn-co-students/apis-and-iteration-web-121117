3e97caeb34f7bbe2a1f670b7ead8fa7fab752b1d
github username <github email address>
2017-12-13 20:30:07 -0500

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -6,7 +6,3 @@ require_relative "../lib/command_line_interface.rb"
 welcome
 users_character_choice = get_character_from_user
 show_character_movies(users_character_choice)
-
-# welcome
-# users_character_choice = get_character_from_user
-# show_character_movies(users_character_choice)
